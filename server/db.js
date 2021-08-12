1adf3f630670fd64199dbd63326c06c0bdb5021c
lavanya <nimushakavilavanya@gmail.com>
2021-08-12 10:32:27 -0700

    reverted changes

--- server/db.js
+++ server/db.js
@@ -10,14 +10,5 @@ const connectDB = async () => {
 
   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
 };
-const Schema  = mongoose.Schema;
-const ProfileSchema =  new Schema({ 
-		FirstName: String,
-		LastName: String,
-		Description: String,
-		Availability: 	Date
 
-	 });
-const Profile = mongoose.model('Profile', ProfileSchema);
-
-module.exports = { connectDB, Profile }; 
+module.exports = connectDB; 
