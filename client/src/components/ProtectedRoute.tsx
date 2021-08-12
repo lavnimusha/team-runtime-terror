1eb7720f13f7988faa2037c41734d3e8e005f644
lavanya <nimushakavilavanya@gmail.com>
2021-08-12 10:10:17 -0700

    addressed changes

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useAuth } from '../context/useAuthContext';
 
-const ProtectedRoute = ({ path, component: Component, ...rest }: { path: string; component: any }) => {
-  const loggedInUser = useAuth();
+const ProtectedRoute = ({ path, component: Component }: { path: string; component: any }) => {
+  const { loggedInUser } = useAuth();
   return (
     <Route
       render={(props) => {
         if (loggedInUser) {
           <Component />;
         } else {
-          return <Redirect to={{ pathname: path, state: { from: props.location } }} />;
+          return <Redirect to={{ pathname: '/login', state: { from: props.location } }} />;
         }
       }}
     />
