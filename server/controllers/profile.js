1905e59ee46a88f8e539a4a8f6ec6ff6d7304689
lavanya <nimushakavilavanya@gmail.com>
2021-08-19 22:00:38 -0700

    minor changes

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -7,7 +7,7 @@ const generateToken = require("../utils/generateToken");
 // @access Private
 
 exports.createProfile = asyncHandler(async (req, res, next) => {
-  console.log(req.body)
+  console.log(req.body);
   const new_obj = Object.assign(
     {
       availability: {
@@ -71,71 +71,78 @@ exports.createProfile = asyncHandler(async (req, res, next) => {
     res.status(400);
     throw new Error("Invalid user data");
   }
-}); 
+});
 
 // @route GET /profiles
 // @desc Search for profiles with query id
 // @access Private
 
 exports.searchProfiles = asyncHandler(async (req, res, next) => {
-    const profile_id = req.query._id;
-   
-    let profiles;
-    if (profile_id) {
-      profiles = await Profile.find({
-        _id: profile_id
-      });
-    }
-  
-    if (!profiles) {
-      res.status(404);
-      throw new Error("No profiles found in search");
-    }
-  
-    res.status(200).json({ profiles: profiles });
-  });  
-  
+  const profile_id = req.query._id;
+
+  let profiles;
+  if (profile_id) {
+    profiles = await Profile.find({
+      _id: profile_id,
+    });
+  }
+
+  if (!profiles) {
+    res.status(404);
+    throw new Error("No profiles found in search");
+  }
+
+  res.status(200).json({ profiles: profiles });
+});
+
 // @route GET /profiles
 // @desc Get a list of profiles
 // @access Private
 
 exports.listAllProfiles = asyncHandler(async (req, res, next) => {
-    
-    let profiles;
-    
-    profiles = await Profile.find({});
-    
-    if (!profiles) {
-      res.status(404);
-      throw new Error("No profiles found in search");
-    }
-  
-    res.status(200).json({ profiles: profiles });
+  let profiles;
+
+  profiles = await Profile.find({});
+
+  if (!profiles) {
+    res.status(404);
+    throw new Error("No profiles found in search");
+  }
+
+  res.status(200).json({ profiles: profiles });
 });
 
 // @route POST /profiles
 // @desc Update profile for given id
 // @access Private
 
-exports.updateProfile = asyncHandler( async(req, res, next) => {
-  
+exports.updateProfile = asyncHandler(async (req, res, next) => {
   const { firstName, lastName, description, email, phoneNumber } = req.query;
   const profile_id = req.query._id;
 
-    const profile = Profile.findOne({_id: profile_id}, function(err) {
-        if(err) {
-            res.status(404).send("Request not found!!");
+  const profile = Profile.findOne({ _id: profile_id }, function (err) {
+    if (err) {
+      res.status(404).send("Request not found!!");
+    }
+  });
+
+  if (profile) {
+    await Profile.updateMany(
+      { _id: profile_id },
+      {
+        firstName: req.query.firstName,
+        lastName: req.query.lastName,
+        description: req.query.description,
+        email: req.query.email,
+        phoneNumber: req.query.phoneNumber,
+      },
+      function (err) {
+        if (err) {
+          res.status(501).send("Internal Server Error!");
         }
 
-    })
-
-    if(profile) {
-        await Profile.updateMany({ _id: profile_id }, { firstName: req.query.firstName, lastName: req.query.lastName, description: req.query.description, email: req.query.email, phoneNumber: req.query.phoneNumber }, function(err) {
-            if(err) {
-                res.status(501).send("Internal Server Error!");
-            }
-
-            res.status(200).send("Request updated successfully");
-        })
-    }
+        res.status(200).send("Request updated successfully");
+      }
+    );
+  }
 });
