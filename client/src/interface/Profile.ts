f6b8850d49a42f45ffd7724c9a5311a378a17c66
lavanya <nimushakavilavanya@gmail.com>
2021-08-26 18:00:51 -0700

    interface for profile

---
+++ client/src/interface/Profile.ts
@@ -0,0 +1,25 @@
+/* export interface IFormInitialValues {
+    firstName: string;
+    lastName: string;
+    email: string;
+    birthDate: string;
+    address: string;
+    phoneNumber: string;
+    description: string;
+    gender: string;
+    availability: {
+      startDate: Date;
+      endDate: Date;
+      daysOfWeek: Array<string>;
+    }; 
+  } */
+export interface IFormInitialValues {
+  firstName: string;
+  lastName: string;
+  email: string;
+  birthDate: string;
+  address: string;
+  phoneNumber: string;
+  description: string;
+  gender: string;
+}
